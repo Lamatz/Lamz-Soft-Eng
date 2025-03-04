e5b6f9f4e27a69902f6148142c8619807d504f63
Lamatz <lamatond444@gmail.com>
2025-03-04 18:31:35 +0800

    integrated the navbar open and close

--- Reports page/Javascript/nav.js
+++ Reports page/Javascript/nav.js
@@ -3,3 +3,8 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector(".sidebar").classList.toggle("expand");
 });
+
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector(".sidebar-col").classList.toggle("shrink");
+});
